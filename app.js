143aa18f389e54e30dab8d0dbaff9f0d4dc9dde4
AbdullahBoreny <abdullah55boreny@gmail.com>
2026-05-17 00:24:06 +0300

    add health check endpoint and configure fly.toml to send requests to to /health path

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const PORT = process.env.PORT || 5001;
 app.use(express.static('dist'));
 
 const start = async () => {
-  await app.listen(PORT+1)
-  console.log(`server started on port ${PORT}`)
-}
+  await app.listen(PORT + 1);
+  console.log(`server started on port ${PORT}`);
+};
 app.get('/version', (req, res) => {
   res.send('1'); // change this string to ensure a new version deployed
 });
-
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw ('error...  ');
+  res.send('ok');
+});
 start();
