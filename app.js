e598c34913a030684aa1564ef40c101f7d5b6820
AbdullahBoreny <abdullah55boreny@gmail.com>
2026-05-17 00:32:07 +0300

    test breaking things again

--- app.js
+++ app.js
@@ -7,15 +7,14 @@ const PORT = process.env.PORT || 5001;
 app.use(express.static('dist'));
 
 const start = async () => {
-  await app.listen(PORT + 1);
+  await app.listen(PORT);
   console.log(`server started on port ${PORT}`);
 };
 app.get('/version', (req, res) => {
   res.send('1'); // change this string to ensure a new version deployed
 });
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw ('error...  ');
+
   res.send('ok');
 });
 start();
